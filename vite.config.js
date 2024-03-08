f1440a94f9314008fd72086861b77fa83c5a6ffb
Vinay0022 <vinu031995@gmail.com>
2024-03-08 21:22:02 +0530

    fixed gh-pages issues, setup the config for vite gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/https://github.com/Vinay0022/image_compressor/",
   plugins: [react()],
 })
