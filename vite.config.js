77834919d5add555648f1f7735b5bf34199d4b94
Vinay0022 <vinu031995@gmail.com>
2024-03-08 22:47:15 +0530

    removed base from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/https://github.com/Vinay0022/image_compressor/",
   plugins: [react()],
 })
