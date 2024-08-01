2b0c105460d0ca932e4401803742f8e0ec1fbf0f
Vinay0022 <vinu031995@gmail.com>
2024-08-02 03:31:40 +0530

    mobile view dropdown background color added & login instead of how to anchor tag name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,25 +11,25 @@ return (
             <header className="bg-[#438181] ">
                 <div className="max-w-4xl mx-auto flex justify-between items-center p-2">
                     <h1 className="hover:cursor-pointer text-center flex  justify-center items-center  font-bold border-black border-2 w-40 h-12 bg-[#D9D9D9] rounded-full" >
-                        <DiDrupal className=" w-9 h-9 inline" /> Compresser
+                        <DiDrupal className=" w-9 h-9 inline" />ToolBox 
                     </h1>
 
                     <nav className="space-x-3 hidden md:block">
                         <a href="/" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:rounded-xl">Home</a>
                         <a href="/image_cropper" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:p-2 rounded-xl">Image Cropper</a>
-                        <a href="" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:p-2 rounded-xl">Go Premium</a>
-                        <a href="" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:p-2 rounded-xl">How To</a>
+                        <a href="/premium" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:p-2 rounded-xl">Go Premium</a>
+                        <a href="/login" className="text-black font-bold text-2xl p-2 hover:bg-[#D9D9D9] hover:p-2 rounded-xl">Login</a>
                     </nav>
                     {dropDown ? <ImCross className='w-10 h-8 md:hidden' onClick={() => { setDropDown(!dropDown) }} /> : <GiHamburgerMenu className='w-10 h-10 md:hidden' onClick={() => { setDropDown(!dropDown) }} />}
                 </div>
             </header>
             {dropDown ?
 
-                <nav className="flex flex-col justify-center align-top top-16 right-0 absolute">
+                <nav className="bg-[#D9D9D9] flex flex-col justify-center align-top top-16 right-0 absolute">
                     <a href="/" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">Home</a>
                     <a href="/image_cropper" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">Image Cropper</a>
-                    <a href="" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">Go Premium</a>
-                    <a href="" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">How To</a>
+                    <a href="/premium" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">Go Premium</a>
+                    <a href="/login" className="text-black font-bold text-xl p-1 hover:bg-[#D9D9D9] rounded-xl">Login</a>
                 </nav>
                 : null
             }
